e0d6554f90aaf16da1f97fe5a3999a1e5130cd5f
Jonathan Blades <jonoblades@gmail.com>
2025-11-12 13:27:45 +0000

    fix: add robust property setter and getter helpers for jsdom/browser environments in PWA tests

--- src/utils/__tests__/pwa.test.ts
+++ src/utils/__tests__/pwa.test.ts
@@ -4,13 +4,54 @@ describe('PWAManager.isPWA', () => {
   const originalMatchMedia = window.matchMedia;
   const originalNavigator = window.navigator;
   const originalReferrer = document.referrer;
-
   afterEach(() => {
     // restore
     Object.defineProperty(window, 'matchMedia', { value: originalMatchMedia, configurable: true });
     Object.defineProperty(window, 'navigator', { value: originalNavigator, configurable: true });
     Object.defineProperty(document, 'referrer', { value: originalReferrer, configurable: true });
   });
+
+  // Helpers to set/restore properties robustly in various jsdom/browser environments
+  function setProp<T extends object, K extends keyof any>(obj: T, prop: K, value: any) {
+    try {
+      Object.defineProperty(obj, prop as string, { value, configurable: true });
+    } catch {
+      try {
+        // fallback
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (obj as any)[prop] = value;
+      } catch {
+        // ignore
+      }
+    }
+  }
+
+  function setGetter<T extends object, K extends keyof any>(obj: T, prop: K, getter: () => any) {
+    try {
+      Object.defineProperty(obj, prop as string, { get: getter, configurable: true });
+    } catch {
+      try {
+        // fallback to value
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (obj as any)[prop] = getter();
+      } catch {
+        // ignore
+      }
+    }
+  }
+
+  function restoreProp<T extends object, K extends keyof any>(obj: T, prop: K, original: any) {
+    try {
+      Object.defineProperty(obj, prop as string, { value: original, configurable: true });
+    } catch {
+      try {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (obj as any)[prop] = original;
+      } catch {
+        // ignore
+      }
+    }
+  }
 
   test('returns true when display-mode is standalone', () => {
     // assign test doubles using Object.defineProperty
